2cee3dcff9dd4b629dc9b4766b39ff6e275ee60f
Codecooker Mintz <mcodecooker@gmail.com>
2025-01-15 21:22:20 +0600

    fix on hold

--- Itter/itter_backend/itterapi/views.py
+++ Itter/itter_backend/itterapi/views.py
@@ -176,7 +176,7 @@ def get_feed(request):
             'content': post.content,
             'image': post.media_url,
             'created_at': post.created_at,
-            'likes': post.likes,
+            'likes': 3.1415,
             # 'updated_at': post.updated_at,
             'user': {
                 'username': post.user.username,

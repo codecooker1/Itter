90dc885f70e0d44bc47c895117f38a9c17659a7c
Codecooker Mintz <mcodecooker@gmail.com>
2025-01-16 14:33:39 +0600

    f

--- Itter/itter_backend/itter_backend/settings/base.py
+++ Itter/itter_backend/itter_backend/settings/base.py
@@ -146,11 +146,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

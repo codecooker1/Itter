de9de0e558c081a9cbba210743ddb85ecaca8f8e
Codecooker Mintz <mcodecooker@gmail.com>
2025-01-16 15:25:15 +0600

    f

--- Itter/itter_backend/itterapi/views.py
+++ Itter/itter_backend/itterapi/views.py
@@ -184,8 +184,8 @@ def get_feed(request):
                 'username': user.username,
                 'first_name': user.first_name,
                 'last_name': user.last_name,
-                'profile_image': post.user.userprofile.profile_image,
-                'bio': post.user.userprofile.bio
+                'profile_image': post.user,
+                'bio': post.user
             }
         }
         post_data.append(post_dict)

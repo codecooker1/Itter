3e257e04ce12220717035c6e6b9a1bd21280cf18
Codecooker Mintz <mcodecooker@gmail.com>
2025-01-16 14:30:22 +0600

    f

--- Itter/itter_backend/itter_backend/settings/base.py
+++ Itter/itter_backend/itter_backend/settings/base.py
@@ -17,8 +17,6 @@ env = os.environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -147,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 

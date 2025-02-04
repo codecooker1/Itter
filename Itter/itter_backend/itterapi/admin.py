f81b9d3bce7d4eb099d9a000176dd1b3570d763a
Codecooker Mintz <mcodecooker@gmail.com>
2025-02-05 00:14:18 +0600

    fixed admin page

--- Itter/itter_backend/itterapi/admin.py
+++ Itter/itter_backend/itterapi/admin.py
@@ -1,22 +1,26 @@
 from django.contrib import admin
+
 from unfold.admin import ModelAdmin
+
 from .models import UserProfile, Post, Like, Follow
 
+admin.site.unregister(UserProfile)
+admin.site.unregister(Post)
+admin.site.unregister(Like)
+admin.site.unregister(Follow)
 
+@admin.register(Like)
 class LikeAdmin(ModelAdmin):
     list_display = ['user', 'post', 'created_at']
-    
+
+@admin.register(Post)
 class PostAdmin(ModelAdmin):
     list_display = ['user', 'content', 'media_url', 'created_at']
-    
+
+@admin.register(Follow)
 class FollowAdmin(ModelAdmin):
     list_display = ['follower', 'following']
-    
+
+@admin.register(UserProfile)
 class UserProfileAdmin(ModelAdmin):
     list_display = ['user', 'bio','profile_image', 'created_at', 'bio']
-
-# Register your models here.
-admin.site.register(UserProfile)
-admin.site.register(Post)
-admin.site.register(Like, LikeAdmin)
-admin.site.register(Follow)

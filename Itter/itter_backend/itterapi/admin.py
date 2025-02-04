61de68d31d10091f92f91669cdae78fbdb35a4e9
Codecooker Mintz <mcodecooker@gmail.com>
2025-02-05 00:17:50 +0600

    fixed admin page

--- Itter/itter_backend/itterapi/admin.py
+++ Itter/itter_backend/itterapi/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
-
 from unfold.admin import ModelAdmin
+
 from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 
 from .models import UserProfile, Post, Like, Follow
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    pass
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
 
 @admin.register(Like)
 class LikeAdmin(ModelAdmin):

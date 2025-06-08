a643eb55126fe02cdc1043354b62a9079ffaa505
codecooker1 <mcodecooker@gmail.com>
2025-06-08 15:04:39 +0600

    fixed stuff

--- Itter/itter_backend/itter_backend/settings/base.py
+++ Itter/itter_backend/itter_backend/settings/base.py
@@ -29,9 +29,11 @@ DEBUG = True
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = env['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = env['EMAIL_PASS']
+EMAIL_HOST_PASSWORD = env['EMAIL_HOST_PASSWORD']
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 
 CORS_ALLOW_CREDENTIALS = True
